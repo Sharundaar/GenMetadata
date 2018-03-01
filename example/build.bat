d307d12a5604f3f8afffced341bba7f45ba4bcbb
Rabiller <drabiller@beenox.com>
2018-03-01 15:47:36 -0500

    build example also create type_db

--- example/build.bat
+++ example/build.bat
@@ -1,5 +1,11 @@
 @echo off
 
+cd ..
+cargo run --release -- example
+copy type_db.* example
+cd example
+
+
 g++ types.cpp -g -c -o types.obj
 g++ type_db.cpp -g -c -o type_db.obj
 g++ main.cpp -g -c -o main.obj
